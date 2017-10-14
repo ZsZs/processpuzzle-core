a57634f503e215429772e38a496c3e5f0959529a
ZsZs <zsolt.zsuffa@gmail.com>
2017-10-14 10:46:43 +0200

    Configure logging for the service.

--- processpuzzle-core-docker/build.sh
+++ processpuzzle-core-docker/build.sh
@@ -2,4 +2,5 @@ rm -fr build
 mkdir build
 cp ../processpuzzle-core-services/target/processpuzzle-core.jar build
 docker build -t zsuffazs/processpuzzle-core .
-docker run -d -p 9125:9125 --name processpuzzle-core zsuffazs/processpuzzle-core
+docker volume create --name=processpuzzle-logs
+docker run -d -p 9125:9125 --volume=processpuzzle-logs:/var/log/processpuzzle-core --name processpuzzle-core zsuffazs/processpuzzle-core
