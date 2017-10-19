cb0afd9ad4e15154e1d650eea13ee808d047f56c
ZsZs <zsolt.zsuffa@gmail.com>
2017-10-19 21:16:50 +0200

    Use Oracle JDK8 image for docker.

--- processpuzzle-core-docker/build.sh
+++ processpuzzle-core-docker/build.sh
@@ -3,4 +3,4 @@ mkdir build
 cp ../processpuzzle-core-services/target/processpuzzle-core.jar build
 docker build -t zsuffazs/processpuzzle-core .
 docker volume create --name=processpuzzle-logs
-docker run -d -p 9125:9125 --volume=processpuzzle-logs:/var/log/processpuzzle-core --name processpuzzle-core zsuffazs/processpuzzle-core
+winpty docker run -it -p 9125:9125 --volume=processpuzzle-logs:/var/log/processpuzzle-core/ --name processpuzzle-core zsuffazs/processpuzzle-core
