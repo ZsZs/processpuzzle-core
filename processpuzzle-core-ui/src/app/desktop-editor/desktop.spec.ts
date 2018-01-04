5816c34ed6a28d755063a5da9492a1f21555dddb
ZsZs <zsolt.zsuffa@gmail.com>
2018-01-04 09:25:51 +0100

    Update UI packages.

---
+++ processpuzzle-core-ui/src/app/desktop-editor/desktop.spec.ts
@@ -0,0 +1,41 @@
+import { Desktop } from './desktop';
+import { BreadCrumb } from './bread-crumb/bread-crumb';
+import { Footer } from './footer/footer';
+
+describe('Desktop', () => {
+   const breadcrumb = new BreadCrumb();
+   const footer = new Footer();
+
+   let desktop: Desktop;
+
+   beforeEach(() => {
+      desktop = new Desktop();
+   });
+
+   it('constructor has no arguments', () => {
+      expect( desktop ).toBeDefined();
+   });
+
+   it('breadcrumb property can be updated', () => {
+      expect( desktop.breadCrumb ).toBeUndefined();
+      desktop.watchDesktopChange().subscribe( message ) => {
+         desktop.updateBreadCrumb( breadcrumb );
+         expect( desktop.breadCrumb ).toBe( breadcrumb );
+         expect( message ).toBe( "")
+      }
+
+   });
+
+   it('footer property can be updated', () => {
+      expect( desktop.footer ).toBeUndefined();
+      desktop.updateFooter( footer );
+      expect( desktop.footer ).toBe( footer );
+   });
+
+   it('properties can be read', () => {
+      desktop.updateBreadCrumb( breadcrumb );
+      desktop.updateFooter( footer );
+      expect( desktop.breadCrumb ).toBe( breadcrumb );
+      expect( desktop.footer ).toBe( footer );
+   });
+});
