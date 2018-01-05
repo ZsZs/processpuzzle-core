0fec09912a7335eae808b0971b7a1ce895debe0d
Zsolt Zsuffa <zsolt.zsuffa@google.com>
2018-01-05 13:31:01 +0000

    test DesktopEditorToolbarComponent.onEditContent()

--- processpuzzle-core-ui/src/app/desktop-editor/desktop-editor-toolbar.component.spec.ts
+++ processpuzzle-core-ui/src/app/desktop-editor/desktop-editor-toolbar.component.spec.ts
@@ -1,13 +1,15 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, tick, TestBed } from '@angular/core/testing';
 
-import { Desktop } from './desktop';
+import { ContentActions, Desktop } from './desktop';
 import { DesktopEditorToolbarComponent } from './desktop-editor-toolbar.component';
 
 describe('DesktopEditorToolbarComponent', () => {
   let component: DesktopEditorToolbarComponent;
+  let desktop: Desktop;
+  let desktopContentActionSpy;
   let fixture: ComponentFixture<DesktopEditorToolbarComponent>;
 
-  beforeEach(async(() => {
+  beforeEach( async(() => {
     TestBed.configureTestingModule({
       declarations: [ DesktopEditorToolbarComponent ],
       providers:    [ Desktop ]
@@ -15,12 +17,27 @@ describe('DesktopEditorToolbarComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DesktopEditorToolbarComponent);
+    fixture = TestBed.createComponent( DesktopEditorToolbarComponent );
     component = fixture.componentInstance;
+    desktop = fixture.debugElement.injector.get( Desktop );
+    desktopContentActionSpy = spyOn( desktop, 'contentAction' );
     fixture.detectChanges();
   });
 
   it('should be created', () => {
-    expect(component).toBeTruthy();
+    expect( component ).toBeTruthy();
   });
+
+  it('onEditContent() notifies desktop about content action', () => {
+     component.onEditContent();
+     expect( desktopContentActionSpy ).toHaveBeenCalledWith( ContentActions.EditContent );
+  });
+
+  it('onShowEditor() outputs onShowEditorEvent', fakeAsync(() => {
+     component.onShowEditorEvent.subscribe(( value: boolean ) => {
+       expect( value ).toBeTruthy();
+    });
+    component.onShowEditor();
+    tick();
+  }));
 });
